b410cce3360a8a11d9861e9e3f9f2513111b06fd
André Silva <andreoew@hotmail.com>
2022-05-15 18:00:42 -0300

    update

--- src/index.js
+++ src/index.js
@@ -105,13 +105,13 @@ app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   const { user } = request;
   const { id } = request.params;
 
-  const todoIndex = user.todos.findIndex(todo => todo.id === id);
+  const todo = user.todos.find(todo => todo.id === id);
 
-  if(!todoIndex === -1) {
+  if(!todo) {
     return response.status(404).json({ error: 'Todo Not Found' });
   }
 
-  user.todos.splice(todoIndex, 1);
+  user.todos.splice(todo);
 
   return response.status(204).json();
 
